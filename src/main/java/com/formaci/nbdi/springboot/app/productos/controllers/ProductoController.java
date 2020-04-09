28cb10acf9b43ceb0aaf6febd3c5197fffd355f9
CristianGaona <crgaonas24@gmail.com>
2020-04-08 21:39:36 -0500

    Establecer el servicio como cliente de Eureka Server y crear varias instacias en el Servidor de registro (Eureka)

--- src/main/java/com/formaci/nbdi/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/formaci/nbdi/springboot/app/productos/controllers/ProductoController.java
@@ -1,9 +1,11 @@
 package com.formaci.nbdi.springboot.app.productos.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,15 +20,28 @@ public class ProductoController {
 	@Autowired
 	private IProductoService productoService;
 	
+	@Autowired
+	private Environment env;
+	
+	@Value("${server.port}")
+	private Integer port;
+	
 	//@GetMapping("/listar")
 	@RequestMapping(method = RequestMethod.GET, value= "/listar")
 	public List<Producto> listar(){
-		return productoService.findAll();
+		return productoService.findAll().stream().map(producto ->{
+			//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			producto.setPort(port);
+			return producto;
+		}).collect(Collectors.toList());
 	}
 	
 	//@GetMapping("/listar/{id}")
-	@RequestMapping(method = RequestMethod.GET, value= "/listar/{id}")
+	@RequestMapping(method = RequestMethod.GET, value= "/ver/{id}")
 	public Producto detalle(@PathVariable Long id) {
+		Producto producto = productoService.findById(id);
+		//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		producto.setPort(port);
 		return productoService.findById(id);
 	}
 }
