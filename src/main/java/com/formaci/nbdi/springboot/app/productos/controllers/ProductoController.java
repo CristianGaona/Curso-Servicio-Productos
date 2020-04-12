544287f4d539777adf0bf02e21823f175156b159
CristianGaona <crgaonas24@gmail.com>
2020-04-11 23:31:20 -0500

    Creando CRUD para el servicio productos

--- src/main/java/com/formaci/nbdi/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/formaci/nbdi/springboot/app/productos/controllers/ProductoController.java
@@ -6,9 +6,15 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formaci.nbdi.springboot.app.productos.models.entity.Producto;
@@ -42,6 +48,45 @@ public class ProductoController {
 		Producto producto = productoService.findById(id);
 		//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 		producto.setPort(port);
-		return productoService.findById(id);
+		//return productoService.findById(id);
+		
+		// Provocando un error
+	/*	boolean ok = false;
+		if(ok == false) {
+			throw new Exception("No se puede cargar el producto");
+		}*/
+		
+		//Realizar una pausa
+		/*try {
+			Thread.sleep(2000L);
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}*/
+		return producto;
 	}
+	
+	@PostMapping("/crear")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto crear(@RequestBody Producto producto) {
+		return productoService.save(producto);
+		
+		
+	}
+	
+	@PutMapping("/editar/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto editar(@RequestBody Producto produto, @PathVariable Long id) {
+		Producto productoDb = productoService.findById(id);
+		productoDb.setNombre(produto.getNombre());
+		productoDb.setPrecio(produto.getPrecio());
+		return productoService.save(productoDb);
+	}
+	
+	@DeleteMapping("/eliminar/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void eliminar(@PathVariable Long id) {
+		productoService.deleteById(id);
+	}
+	
 }
