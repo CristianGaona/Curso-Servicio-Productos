6887d45e36920a3fdbbe80703280ad83265e37ee
Cristian Gaona <crgaonas24@gmail.com>
2020-05-01 16:46:22 -0500

    Documentación de una API REST de Spring con OpenAPI 3.0

--- src/main/java/com/formaci/nbdi/springboot/app/productos/CursoServicioProductosApplication.java
+++ src/main/java/com/formaci/nbdi/springboot/app/productos/CursoServicioProductosApplication.java
@@ -5,8 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @EnableEurekaClient
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Producto API", version = "2.0", description = "Microservicio Productos"))
 @EntityScan("com.formaci.nbdi.springboot.app.commons.models.entity")
 public class CursoServicioProductosApplication {
 
